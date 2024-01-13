4eabe8f005db37c6a2187f4ec7996961a4430a15
Dev_Nergis <136839304+DevNergis@users.noreply.github.com>
2024-01-13 13:47:33 +0000

    asdkjhsdfkfjhs

--- v1/ipfs/ipfs.py
+++ v1/ipfs/ipfs.py
@@ -36,13 +36,14 @@ async def ipfs_upload(files: List[UploadFile] = File()):
 
     async with aiohttp.ClientSession(headers=header) as session:
         async with session.post("https://api.nft.storage/upload", data=file_list) as response:
-            data = await response.json()
-
-    cid = data["value"]["cid"]
-    directory_url = f"https://{cid}.ipfs.cf-ipfs.com"
-
-    for file_name in file_name_list:
-        file_direct_list.append(f"https://{cid}.ipfs.cf-ipfs.com/{file_name}")
+            try:
+                data = await response.json()
+                cid = data["value"]["cid"]
+                directory_url = f"https://{cid}.ipfs.cf-ipfs.com"
+                for file_name in file_name_list:
+                    file_direct_list.append(f"https://{cid}.ipfs.cf-ipfs.com/{file_name}")
+            except:
+                pass
 
     if response.status == 200:
         return ORJSONResponse(content={"file_size": file_size_list, "directory_cid": cid, "file_name": file_name_list, "directory_url": directory_url, "file_direct": file_direct_list}, status_code=200)
