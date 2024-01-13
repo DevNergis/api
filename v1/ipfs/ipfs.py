04ee025da2ccf93c61dd8cba000c4ca52839c7de
Dev_Nergis <136839304+DevNergis@users.noreply.github.com>
2024-01-13 13:04:56 +0000

    ghhghg

--- v1/ipfs/ipfs.py
+++ v1/ipfs/ipfs.py
@@ -1,13 +1,14 @@
 from main import *
 from src.function import *
 from src.schema import *
+from math import ceil
+from io import BytesIO
+import aiohttp
 
 router = APIRouter(prefix="/v1/ipfs", tags=["ipfs"])
 
 @router.post("/upload")
 async def ipfs_upload(files: List[UploadFile] = File()):
-    import requests
-
     file_size_list = list()
     file_name_list = list()
     file_direct_list = list()
@@ -17,16 +18,25 @@ async def ipfs_upload(files: List[UploadFile] = File()):
         'Authorization': Authorization
     }
 
-    file_list = list()
+    file_list = aiohttp.FormData()
 
     for file in files:
-        file_list.append(('file', (file.filename, await file.file.read(2*1024*1024), 'application/octet-stream')))
+        chunk = BytesIO()
+        chunk_range = range(ceil(file.size / (1024*1024*2)))
+        
+        for _ in chunk_range:
+            chunk.write(await file.read(1024*1024*2))
+
+        chunk.seek(0)
+
+        file_list.add_field(name="file", value=chunk.read(), content_type="application/octet-stream", filename=file.filename)
 
         file_size_list.append(file.size)
         file_name_list.append(file.filename)
 
-    response = requests.post("https://api.nft.storage/upload", files=file_list, headers=header)
-    data = response.json()
+    async with aiohttp.ClientSession(headers=header) as session:
+        async with session.post("https://api.nft.storage/upload", data=file_list) as response:
+            data = await response.json()
 
     cid = data["value"]["cid"]
     directory_url = f"https://{cid}.ipfs.cf-ipfs.com"
@@ -34,5 +44,7 @@ async def ipfs_upload(files: List[UploadFile] = File()):
     for file_name in file_name_list:
         file_direct_list.append(f"https://{cid}.ipfs.cf-ipfs.com/{file_name}")
 
-    if response.status_code == 200:
+    if response.status == 200:
         return ORJSONResponse(content={"file_size": file_size_list, "directory_cid": cid, "file_name": file_name_list, "directory_url": directory_url, "file_direct": file_direct_list}, status_code=200)
+    else:
+        return PlainTextResponse(content=await response.read(), status_code=response.status)
