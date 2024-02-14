60bfb551b8e71e713902cb7f0eec29e2ee6f157d
Dev_Nergis <136839304+DevNergis@users.noreply.github.com>
2024-02-14 12:25:12 +0000

    asd

--- v1/file/file.py
+++ v1/file/file.py
@@ -3,30 +3,30 @@ from main import *
 from src.function import *
 from src.schema import *
 import aiofiles
+from fastapi.security.api_key import APIKeyHeader
+from fastapi import Security
 
 router = APIRouter(prefix="/v1/file", tags=["file"])
 
-@router.get("/download/{file_id}")
-async def file_download(file_id: str, file: Union[str, None] = None, password: Union[str, None] = "password"):
-    redis_file_db_password = redis.Redis(connection_pool=pool(PASSWORD_DB))
-    password_db = redis_file_db_password.get(file_id).decode('utf-8')
-    redis_file_db_password.close()
-    password_db = bytes.fromhex(password_db).decode('utf-8')
-    password_db = base64.b64decode(password_db).decode("utf-8")
+password_header = APIKeyHeader(name="x-password", auto_error=False)
 
-    if password == "password":
-        if password == password_db:
-            pass
-        else:
+@router.get("/download/{file_id}")
+async def file_download(file_id: str, file: Union[str, None] = None, password: Union[str, None] = Security(password_header)):
+    try:
+        redis_file_db_password = redis.Redis(connection_pool=pool(PASSWORD_DB))
+        password_db = redis_file_db_password.get(file_id).decode('utf-8')
+        redis_file_db_password.close()
+        password_db = bytes.fromhex(password_db).decode('utf-8')
+        password_db = base64.b64decode(password_db).decode("utf-8")
+
+        if password == None:
             return HTMLResponse("Need Password", status_code=403)
-    else:
-        if password == password_db:
+        elif password == password_db:
             pass
         else:
-            if password_db == "password":
-                return HTMLResponse("No Password Required", status_code=403)
-            else:
-                return HTMLResponse("Wrong Password", status_code=403)
+            return HTMLResponse("Wrong Password", status_code=403)
+    except:
+        pass
 
     if file == None:
         redis_file_db_name = redis.Redis(connection_pool=pool(FILE_DB))
@@ -40,7 +40,7 @@ async def file_download(file_id: str, file: Union[str, None] = None, password: U
         return FileResponse(f"{FILE_PATH}/{file_id}", filename=file)
 
 @router.post("/upload")
-async def file_upload(files: List[UploadFile] = File(), password: Union[str, None] = None):
+async def file_upload(files: List[UploadFile] = File(), password: Union[str, None] = Security(password_header)):
     file_size_list = list()
     file_uuid_list = list()
     file_name_list = list()
@@ -56,19 +56,12 @@ async def file_upload(files: List[UploadFile] = File(), password: Union[str, Non
         file_name_list.append(file.filename)
         file_url_list.append(f"{SERVER_URL}/v1/file/download/{file_uuid}")
         file_direct_list.append(f"{SERVER_URL}/v1/file/download/{file_uuid}/?file={file.filename}")
-        if password == "password":
+
+        if password == None:
             password_status = "No"
         else:
             password_status = "Yes"
-
-        if password != None:
             password = base64.b64encode(bytes(password, 'utf-8')).hex()
-
-            redis_file_db_password = redis.Redis(connection_pool=pool(PASSWORD_DB))
-            redis_file_db_password.set(file_uuid, password)
-            redis_file_db_password.close()
-        else:
-            password = base64.b64encode(bytes("password", 'utf-8')).hex()
 
             redis_file_db_password = redis.Redis(connection_pool=pool(PASSWORD_DB))
             redis_file_db_password.set(file_uuid, password)
