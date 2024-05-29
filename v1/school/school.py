a38b28b364e8f9b929be7603b6b0d8f8a2717555
Dev_Nergis <136839304+DevNergis@users.noreply.github.com>
2024-05-29 12:31:07 +0900

    aads

--- v1/school/school.py
+++ v1/school/school.py
@@ -16,6 +16,7 @@ async def mealservicedietinfo(body: mealservicedietinfo_):
     atpt_ofcdc_sc_code = rrr['ATPT_OFCDC_SC_CODE']
     sd_schul_code = rrr['SD_SCHUL_CODE']
     url = f"https://open.neis.go.kr/hub/mealServiceDietInfo?KEY={OPEN_NEIS_API_KEY}&Type=json&pIndex=1&pSize=10&ATPT_OFCDC_SC_CODE={atpt_ofcdc_sc_code}&SD_SCHUL_CODE={sd_schul_code}&MLSV_YMD={DATE}"
+    print(url)
     request = requests.get(url=url)
     if request.text == """{"RESULT":{"CODE":"INFO-200","MESSAGE":"해당하는 데이터가 없습니다."}}""":
         return ORJSONResponse(content={"RESULT":{"CODE":"INFO-200","MESSAGE":"해당하는 데이터가 없습니다."}}, status_code=200)
