c1b46e8398d004ab6f57f9fd7fd376200a845ff9
Dev_Nergis <136839304+DevNergis@users.noreply.github.com>
2024-06-08 18:44:39 +0900

    date up

--- v1/school/school.py
+++ v1/school/school.py
@@ -6,8 +6,8 @@ import requests
 router = APIRouter(prefix="/v1/school", tags=["school"])
 
 
-# noinspection SpellCheckingInspection
-@router.post("/meal")
+# noinspection SpellCheckingInspection,PyUnboundLocalVariable
+@router.post("/meal", description="날자 형식: yyyy-mm-dd, ex) 2024-06-17")
 async def meal_info(body: SchemaMealInfo):
     url = f"https://open.neis.go.kr/hub/schoolInfo?KEY={OPEN_NEIS_API_KEY}&Type=json&pIndex=1&pSize=10&SCHUL_NM={body.school_name}"
     request = requests.get(url=url)
@@ -18,8 +18,11 @@ async def meal_info(body: SchemaMealInfo):
     sd_schul_code = rrr['SD_SCHUL_CODE']
     print(atpt_ofcdc_sc_code)
     print(sd_schul_code)
-    url = f"https://open.neis.go.kr/hub/mealServiceDietInfo?KEY={OPEN_NEIS_API_KEY}&Type=json&pIndex=1&pSize=10&ATPT_OFCDC_SC_CODE={atpt_ofcdc_sc_code}&SD_SCHUL_CODE={sd_schul_code}&MLSV_FROM_YMD={DATE}&MLSV_TO_YMD={DATE}"
-    print(url)
+
+    if body.date:
+        body.date = DATE
+
+    url = f"https://open.neis.go.kr/hub/mealServiceDietInfo?KEY={OPEN_NEIS_API_KEY}&Type=json&pIndex=1&pSize=10&ATPT_OFCDC_SC_CODE={atpt_ofcdc_sc_code}&SD_SCHUL_CODE={sd_schul_code}&MLSV_FROM_YMD={body.date}&MLSV_TO_YMD={body.date}"
     request = requests.get(url=url)
     if request.text == """{"RESULT":{"CODE":"INFO-200","MESSAGE":"해당하는 데이터가 없습니다."}}""":
         return ORJSONResponse(content={"RESULT": {"CODE": "INFO-200", "MESSAGE": "해당하는 데이터가 없습니다."}}, status_code=200)
