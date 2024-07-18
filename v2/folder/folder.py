2c6b9c0a6732a5faffe23180a8e56c7f5d2033d0
Dev_Nergis <136839304+DevNergis@users.noreply.github.com>
2024-07-18 21:57:44 +0900

    artists

--- v2/folder/folder.py
+++ v2/folder/folder.py
@@ -76,7 +76,7 @@ async def folder_open(folder_id: str):
 @router.post("/{folder_id}/upload")
 async def folder_upload(folder_id: str, files: List[UploadFile] = File(),
                         folder_password: Union[str, None] = Security(folder_password),
-                        folder_admin_password: Union[str, None] = Security(folder_admin_password)):
+                        folder_admin_password: Union[str, None] = folder_admin_password):
     file_uuid_list: list = []
     print(folder_password)
     print(folder_admin_password)
