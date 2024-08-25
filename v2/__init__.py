491a04514161d976de9ab1186b11347fd9ba411b
DevNergis <sns77103310@gmail.com>
2024-08-25 19:34:23 +0900

    asd

--- v2/__init__.py
+++ v2/__init__.py
@@ -1,13 +1,11 @@
 from fastapi import *
 
-from .file import file
 from .folder import folder
 from .cipher import cipher
 from .school import school
 
 router = APIRouter(prefix="/v2", tags=["v2"])
 
-router.include_router(file.router)
 router.include_router(folder.router)
 router.include_router(cipher.router)
 router.include_router(school.router)
